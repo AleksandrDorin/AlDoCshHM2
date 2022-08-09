dde425e61bb7ea226afdda88cf049452f9efe9db
AleksandrDorin <dorinaleksandrspb@yandex.ru>
2022-08-09 23:53:26 +0300

    Task_05 completed

---
+++ Task_05/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти кубы чисел от 1 до N
+
+Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+int a = 1;
+
+while (a <= n)
+{
+    // int res = a * a * a;
+    int res = Convert.ToInt32(Math.Pow(a, 3));
+    a++;
+    Console.WriteLine(res++);
+}
