0e151d317e49aa5f3cdda6b38612e5568b46e552
AleksandrDorin <dorinaleksandrspb@yandex.ru>
2022-08-10 14:37:14 +0300

    Task_06 completed

---
+++ Task_06/Program.cs
@@ -0,0 +1,16 @@
+﻿// Найти сумму чисел от 1 до А
+
+Console.WriteLine("Введите число А: ");
+int a = int.Parse(Console.ReadLine());
+int i = 1;
+int sum = 0;
+
+for(i = 1; i <= a ; i += 1)
+{
+    sum += i;
+    Console.WriteLine(i);
+}
+
+Console.WriteLine( sum );
+
+
