f92c36f1ef74aae93717839e4444210b55c09a6f
AleksandrDorin <dorinaleksandrspb@yandex.ru>
2022-08-10 14:59:11 +0300

    Task_07 completed

---
+++ Task_07/Program.cs
@@ -0,0 +1,14 @@
+﻿// Определить количество цифр в числе
+
+Console.WriteLine("Введите число: ");
+string a = Console.ReadLine();
+int i = 0;
+
+while(i < a.Length)
+{
+    i += 1;
+}
+
+Console.WriteLine("Число "+ a +" содержит в себе " + i + " цифр");
+
+
