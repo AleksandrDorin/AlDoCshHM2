5afba740f6a3ab272cde5b17b9cc978417148bc2
AleksandrDorin <dorinaleksandrspb@yandex.ru>
2022-08-10 16:55:36 +0300

    Task_09 completed

---
+++ Task_09/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+Console.WriteLine("Введите число А: ");
+int a = int.Parse(Console.ReadLine());
+int i = 1;
+double pow = 1;
+
+for(i = 1; i <= a ; i += 1)
+{
+    pow *= i;
+    Console.WriteLine(i);
+  
+}
+
+Console.WriteLine("Произведение чисел будет равно " + pow );
