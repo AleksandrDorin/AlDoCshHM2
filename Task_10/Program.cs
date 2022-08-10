b8e728b0aa1759e162ed79978429bc03a661231e
AleksandrDorin <dorinaleksandrspb@yandex.ru>
2022-08-10 17:56:06 +0300

    Task_10 completed

---
+++ Task_10/Program.cs
@@ -0,0 +1,18 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+int a = 1;
+
+while (a <= n)
+{
+    
+    int res = Convert.ToInt32(Math.Pow(a, 3));
+    
+    if (res % 2 == 0)
+    {
+        Console.WriteLine(res++);
+    }
+
+    a++;
+}
